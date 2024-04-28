62f8e2540f19d5d2c594b486001958ebdab08fa1
Stefanus Nanami <saint_nus@mac.com>
2024-04-29 08:21:04 +0900

    Initial commit.

---
+++ main.py
@@ -0,0 +1,24 @@
+# Main function.
+
+import compiledb as cdb
+import os.path as fpath
+import argparse
+
+def main(compileCommands: str):
+    cdb.loadCompileCommands(compileCommands)
+
+def checkFile(path: str, parser: argparse.ArgumentParser):
+    if not fpath.isfile(path):
+        parser.error("File %s not found or invalid.", path)
+    else:
+        return path
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='C/CPP static analyzer using clang-tidy.')
+    parser.add_argument('input_file',
+                        type=lambda file_path: checkFile(path=file_path, parser=parser),
+                        help='Compile commands to load.')
+
+    args = parser.parse_args()
+
+    main(args.input_file)
